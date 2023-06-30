f4dcf6499cb77faf228156e83c25a81fb694f665
akhlakm <me@akhlakm.com>
2023-06-29 21:42:09 -0400

    Add bump

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+bump() {
+    ## Bump the version number
+    grep version pyproject.toml
+    VERSION=$(sed -n 's/version = "\(.*\)"/\1/p' pyproject.toml)
+    VERSION=$(python -c "v='$VERSION'.split('.');print('%s.%s.%d' %(v[0], v[1], int(v[2])+1))")
+    echo "   >>>"
+    sed -i "s/\(version = \"\)[^\"]*\"/\1$VERSION\"/" pyproject.toml
+    sed -i "s/\(__version__ = \"\)[^\"]*\"/\1$VERSION\"/" pylogg/__init__.py
+    grep version pyproject.toml
+    git add pyproject.toml pylogg/__init__.py
+    git commit -m "Bump to version $VERSION"
+}
+
 version() {
     # print current version
     grep version pyproject.toml

85eda831e4fd73c9f7bcc60ffa288a5c47b8ada9
akhlakm <me@akhlakm.com>
2024-02-22 11:39:52 -0500

    Rename build.sh to make.sh, use publish

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bump() {
+publish() {
     ## Bump the version number
     grep version pyproject.toml
     VERSION=$(sed -n 's/version = "\(.*\)"/\1/p' pyproject.toml)
@@ -10,23 +10,16 @@ bump() {
     sed -i "s/\(__version__ = \"\)[^\"]*\"/\1$VERSION\"/" pylogg/__init__.py
     grep version pyproject.toml
     git add pyproject.toml pylogg/__init__.py
-}
 
-version() {
-    # print current version
-    grep version pyproject.toml
-    read -p "new version string? " NEW_VERSION
-    sed -i "s/\(version = \"\)[^\"]*\"/\1$NEW_VERSION\"/" pyproject.toml
-    sed -i "s/\(__version__ = \"\)[^\"]*\"/\1$NEW_VERSION\"/" pylogg/__init__.py
-    # confirm
-    grep version pyproject.toml
-}
+    git commit -m "Bump version"
+    git push
 
-tag() {
-    # create a new git tag using the pyproject.toml version
+    # Create a new git tag using the pyproject.toml version
     # and push the tag to origin
     version=$(sed -n 's/version = "\(.*\)"/\1/p' pyproject.toml)
     git tag v$version && git push origin v$version
+
+    echo "OK"
 }
 
 prepare() {

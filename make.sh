8910074525ca43f6f28d4d717983a5ddc15a1dfa
akhlakm <me@akhlakm.com>
2024-02-26 23:26:50 -0500

    Update make file to setup env

--- make.sh
+++ make.sh
@@ -1,4 +1,32 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#           ---   Makefile like bash script for development    ---
+#  --- Source this script in your terminal to set up the dev environment ---
+#      --- Execute the script to see a list of available commands ---
+# ------------------------------------------------------------------------------
+
+[[ -n $MKWD ]] || export MKWD=$(echo $(command cd $(dirname '$0'); pwd))
+CONDAENV=python-logg
+
+## -----------------------------------------------------------------------------
+
+if [[ $(basename "${0}") != "make.sh" ]]; then
+    # Script sourced, load or create condaenv
+    if ! conda activate $CONDAENV; then
+        # Create a conda environment.
+        echo "Setting up $CONDAENV conda environment."
+        conda create -n $CONDAENV python=3.10 || return 10
+        conda activate $CONDAENV || return 11
+    fi
+
+    export PYTHONPATH=$MKWD
+
+    alias cdmk="cd $MKWD/"
+    alias mk="$MKWD/make.sh"
+    echo "Environment set up. You can now use 'mk' to execute this script."
+
+    return 0
+fi
+
 
 publish() {
     # Run tests
@@ -26,8 +54,36 @@ publish() {
     echo "OK"
 }
 
-prepare() {
-    pre-commit run --all-files
+setup() {
+    pip install pytest pre-commit
+    pre-commit install
 }
 
-"$@"
+prepare() {
+    pre-commit run --all-files
+    pytest
+}
+
+test() {
+    pytest -vv -s
+}
+
+## EXECUTE OR SHOW USAGE.
+## -----------------------------------------------------------------------------
+if [[ "$#" -lt 1 ]]; then
+    echo -e "\nUSAGE:  mk <command> [options ...]"
+    echo -e "\tSource this script to setup the terminal environment."
+    echo -e "\nAvailable commands:"
+    echo -e "------------------------------------------------------------------"
+    echo -e "    setup      Install dev requirements and setup env."
+    echo
+    echo -e "    prepare    Prepare files to publish."
+    echo
+    echo -e "    test       Run tests."
+    echo
+    echo -e "    publish    Publish current changes as a new version."
+    echo
+else
+    cd $MKWD
+    "$@"
+fi
